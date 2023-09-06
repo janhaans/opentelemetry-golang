b319cd6bf90b1658c3fed7e0f96d00c3423ce0ae
Jan Haans <jan.haans@gmail.com>
2023-09-06 19:57:23 +0200

    Make application interruptable

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
+	"os/signal"
 
 	"github.com/janhaans/opentelemetry-golang/app"
 )
@@ -14,8 +14,22 @@ func main() {
 	i := os.Stdin
 	l := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
 	a := app.NewApp(ctx, i, l)
-	err := a.Run(ctx)
-	if err != nil {
-		fmt.Printf("%v\n", err)
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+	errCh := make(chan error)
+
+	go func() {
+		errCh <- a.Run(ctx)
+	}()
+
+	select {
+	case <-sigCh:
+		l.Println("\nGoodbye")
+		return
+	case err := <-errCh:
+		if err != nil {
+			l.Fatalf("%v\n", err)
+		}
 	}
 }
